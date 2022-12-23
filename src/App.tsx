18c76f900098eab493804ca5219e23987dba1988
Janyelle Nayara <71035622+JanyelleNayara@users.noreply.github.com>
2022-12-23 19:55:43 +0000

    Configuração inicial do jogo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+type Players = "O" | "X";
 
 export default function App() {
+  const [turn, setTurn] = useState<Players>("O");
+  const [winner, setWinner] = useState<Players | null>(null);
+  const [draw, setDraw] = useState<boolean | null>(null);
+  const [marks, setMarks] = useState<{ [key: string]: Players }>({});
+
+  const getSquares = () => {
+    return new Array(9).fill(true);
+  };
+
+  const play = (index: number) => {
+    setMarks((prev) => ({ ...prev, [index]: turn }));
+    setTurn((prev) => (prev === "O" ? "X" : "O"));
+  };
+
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
-  )
+    <div className="game">
+
+      <div className="board">
+        {getSquares().map((_, index) => (
+          <div className="boardSquare" onClick={() => play(index)}>
+            {marks[index]}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
+
